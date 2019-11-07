998ee2a2fe44a7c653ecaec2457f80c4929daf4d
Kevin Downs <kevin.downs04@gmail.com>
2019-11-07 00:04:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -36,7 +36,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  binding.pry
   holiday_hash[:winter].values.flatten
 end
 
