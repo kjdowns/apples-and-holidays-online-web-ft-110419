fb8c339e1e729619c4d7a977a2b1ca57982f1b17
Kevin Downs <kevin.downs04@gmail.com>
2019-11-07 00:20:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,6 +39,11 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
 
+def cap(item)
+  cap_array = item.to_s.split("_")
+  
+end
+
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
